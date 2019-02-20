eeb062077a877f6895febbd0a61b808a5bff1b51
Jian Ming <jianming@iMac-Laptop.local>
2019-02-20 09:51:52 +0800

    Create username, email and password fields. Specify constraints

--- models/user.py
+++ models/user.py
@@ -2,8 +2,9 @@ from models.base_model import BaseModel
 import peewee as pw
 
 
+
 class User(BaseModel):
     name = pw.CharField(unique=False)
-    # email=pw.TextField(unique=True)
-    # username=pw.TextField(unique=True)
-    # pass
+    email=pw.CharField(max_length=128, unique=True)
+    username=pw.CharField(max_length=128, unique=True, index=False)  #Index: search by username
+    password=pw.CharField(max_length=128)
